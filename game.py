8d0736a82aecf5832b57683144f373c948f58407
iuncus <gscsd111@gmail.com>
2025-08-06 02:30:19 -0400

    updated trajectory

--- game.py
+++ game.py
@@ -17,6 +17,7 @@ sun = body(10000, (0, 0), (width//2, height//2))
 bodies_list = [sun]
 sim = simulation(bodies_list)
 mouse_down = False
+ghost_bodies = []
 while not exit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -31,7 +32,7 @@ while not exit:
         elif event.type == pygame.MOUSEBUTTONUP:
             release_coord = pygame.mouse.get_pos()
             # velocity = ((release_coord[0] - click_coord[0]) //10 , (release_coord[1] - click_coord[1]) //10 ) 
-            velocity = ((click_coord[0] - release_coord[0]) //5 , (click_coord[1] - release_coord[1]) //10 ) 
+            velocity = ((click_coord[0] - release_coord[0]) //10 , (click_coord[1] - release_coord[1]) //10 ) 
             
 
             planet = body(10, velocity, click_coord)
@@ -47,8 +48,28 @@ while not exit:
     if mouse_down and click_coord:
         pygame.draw.circle(canvas, blue, click_coord, 5)
         mouse_pos = pygame.mouse.get_pos()
+        # ghost_bodies = bodies_list.copy()
+        ghost_bodies = [body(b.mass, b.velocity, b.position) for b in bodies_list]
+        velocity = ((click_coord[0] - mouse_pos[0]) //10 , (click_coord[1] - mouse_pos[1]) //10 )
+        ghost = body(10, velocity, click_coord)
+        ghost_bodies.append(ghost)
+        ghost_sim = simulation(ghost_bodies)
+
+
+
         if click_coord != mouse_pos:
-            pygame.draw.line(canvas, blue, click_coord, ((click_coord[0] * 2 - mouse_pos[0]), ((click_coord[1] * 2 - mouse_pos[1]))))
+            trajectory = []
+            for i in range(300):
+                pos = ghost_bodies[-1].position.copy()
+                ghost_sim.step()
+                # new_pos = ghost_body.position
+                trajectory.append(pos)
+
+            pygame.draw.lines(canvas, blue, False, trajectory, 1)
+            # print(trajectory)
+                # pygame.draw.line(canvas, blue, click_coord, ((click_coord[0] * 2 - mouse_pos[0]), ((click_coord[1] * 2 - mouse_pos[1]))))
+
+
 
 
 
